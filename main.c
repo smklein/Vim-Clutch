15c96de22c8ebbc43bbbe4d09d40706cd739c926
utkartsh <utkartsh@utkartsh-900SD.(none)>
2013-01-14 20:17:14 -0500

    just did it

--- main.c
+++ main.c
@@ -1,9 +1,20 @@
-//#include "footswitch.c"
+#include "footswitch.c"
 //#include "common.c"
 //#include "debug.c"
 
 int main()
 {
-    printf("feafeag");
+/*
+    init();
+    read_pedals();
+    deinit();*/
+    
+
+    init_pedals();
+    curr_pedal = &pd.pedals[1];
+    compile_key("i");
+    init();
+    write_pedals();
+    deinit();
     return 0;
 }
